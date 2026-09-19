e0e6cac0f7eb7273d8b5bf5d6cc6a8717f42aab4
victor-kipyegon-koech <victorkipyegon778@gmail.com>
2026-09-19 11:06:50 +0300

    added the database connection

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import pg from "pg"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const{pool}=pg;
+
+export const pool =new pool({
+    user:process.env.PGUSER,
+    host:process.env.PGHOST,
+    database:process.env.PGDATABASE,
+    password:process.env.PGPASSWORD,
+    port:Number(process.env.PGPORT),
+    max:20,//max connections in the pool
+    idleTimeoutMillis:30000,
+    connectionTimeoutMillis:2000
+});
+pool.on('connect',()=>{
+    console.log('Database connected successfully');
+});
